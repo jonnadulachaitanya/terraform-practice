c5e7f5603385f209d7ada4e488e966ebd7bbb6c6
chaitanyajonnadula <chaitanyajonnadula98@gmail.com>
2024-12-18 07:58:48 +0530

    file committed

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -17,4 +17,8 @@ resource "aws_security_group" "allow_ssh_terraform" {
         cidr_blocks = ["0.0.0.0/0"]
         ipv6_cidr_blocks = ["::/0"]
     }
+
+    tags  = {
+        Name = "allow_ssh"
+    }
 }
