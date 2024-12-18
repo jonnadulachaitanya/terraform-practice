b60df77665d5684120e710933359043cecf19617
chaitanyajonnadula <chaitanyajonnadula98@gmail.com>
2024-12-18 07:49:29 +0530

    file committed

---
+++ ec2/ec2.tf
@@ -0,0 +1,20 @@
+resource "aws_security_group" "allow_ssh_terraform" {
+    description = "allow port number 22 for ssh"
+    name = "allow_ssh"
+    
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+}
