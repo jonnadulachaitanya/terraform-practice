ce2d8c214129514fc00df707b386e9f86ddf517f
chaitanyajonnadula <chaitanyajonnadula98@gmail.com>
2024-12-19 09:16:37 +0530

    file committed

---
+++ variables/ec2.tf
@@ -0,0 +1,29 @@
+resource "aws_security_group" "allow-ssh_terraform" {
+    name = var.name
+    description = var.description
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+
+
+    }
+
+    ingress {
+        from_port = var.from_port
+        to_port = var.to_port
+        protocol = var.protocol
+        cidr_blocks = var.ingress_cidr_blocks]
+        ipv6-cidr-blocks = var.ingress_ipv6_cidr_blocks
+    }
+}
+
+resource "aws_instance" "terraform" {
+    ami_id = var.ami_id
+    instance_type = var.instance_type
+    vpc_security_group_ids = [aws_security_group.allow_ssh_terraform.id]
+    tags = var.tags
+}
---
+++ variables/variables.tf
@@ -0,0 +1,57 @@
+variable "ami_id" {
+    default = "ami-09c813fb71547fc4f"
+    type = string
+
+}
+variable "instance_type" {
+    default = "t3.micro"
+    type = string
+}
+
+variable "tags" {
+    type =map
+    default = {
+        Name = "terraform"
+        Environment = "Development"
+        terraform = "true"
+        project = "expense"
+        component = "backend"
+    }
+}
+
+variable "name" {
+    default = "allow_ssh"
+    type = string
+
+}
+
+variable "description" {
+    default = "port 22 for accessing ssh"
+    type = string
+}
+
+variable "from_port" {
+    default = "22"
+    type = number
+}
+
+variable "to_port" {
+    default = "22"
+    type =  number
+}
+
+variable "protocol" {
+    default = "tcp"
+    type = string
+}
+
+variable "ingress_cidr_blocks" {
+    default = ["0.0.0.0./0"]
+    type = list(string)
+
+}
+
+variable "ingress_ipv6_cidr_blocks" {
+    default = ["::/0"]
+    type = list(string)
+}
