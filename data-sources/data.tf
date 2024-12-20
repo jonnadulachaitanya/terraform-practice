94663830fe0505cbfd2dd8f2d1481a22f9844266
chaitanyajonnadula <chaitanyajonnadula98@gmail.com>
2024-12-20 13:13:44 +0530

    Add files via upload

--- data-sources/data.tf
+++ data-sources/data.tf
@@ -1,1 +1,20 @@
+data "aws_ami" "join-devops" {
+    most_recent = true
+    owners = ["973714476881"]
 
+    filter {
+        name = "name"
+        values = ["RHEL-9-DevOps-Practice"]
+    }
+
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+
+    }
+
+    filter {
+        name = "root-device-type"
+        values = ["ebs"]
+    }
+}
